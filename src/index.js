8406ff704b08e8f2b65986cc9014151036b61f3e
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2019-11-14 14:26:21 -0700

    reverting to working version

--- src/index.js
+++ src/index.js
@@ -186,15 +186,11 @@ class ReactPhoneInput extends React.Component {
   };
 
   replaceTextHoldingCursor = (target, search, replaceWith) => {
-    // The additional check of length is added for not performing the following task on Delete of characters
-    if (replaceWith.indexOf(search) >= 0 && replaceWith.length <= search.length) {
-      
+    if (target.value.indexOf(search) >= 0) {
       const start = target.selectionStart;
       const end = target.selectionEnd;
-      const textBefore = replaceWith.substr(0, end);
+      const textBefore = target.value.substr(0, end);
       const lengthDiff = (replaceWith.length - search.length) * this.getCountOfReplacedText(textBefore, search);
-
-      //We want to update input with new value
       target.value = target.value.replace(search, replaceWith);
       target.selectionStart = start + lengthDiff;
       target.selectionEnd = end + lengthDiff;
@@ -202,26 +198,14 @@ class ReactPhoneInput extends React.Component {
   };
 
   componentDidUpdate() {
-    if (this.props.value !== this.state.formattedNumber && this.props.value !== `+${this.state.formattedNumber}`) {
+    if (this.props.value !== this.state.formattedNumber) {
+      const input = this.numberInputRef;
 
-      // Logic for handling the value update on some other component changes
-      if (this.props.value.length < 5 && this.state.formattedNumber.length > 9 && this.state.formattedNumber.startsWith(this.props.value)) {
-        this.setState({
-          formattedNumber: this.props.value
-        });
-      } else {
-        const input = this.numberInputRef;
-
-        // This part is implemented to hold the cursor when editing number
-        // Solution found from https://stackoverflow.com/questions/40196467/restore-cursor-position-after-replace
-        this.replaceTextHoldingCursor(input, this.state.formattedNumber, this.props.value);
-        this.setState({
-          formattedNumber: input.value,
-        });
-      }
-    } else if (this.props.value === `+${this.state.formattedNumber}`) {
+      // This part is implemented to hold the cursor when editing number
+      // Solution found from https://stackoverflow.com/questions/40196467/restore-cursor-position-after-replace
+      this.replaceTextHoldingCursor(input, this.state.formattedNumber, this.props.value);
       this.setState({
-        formattedNumber: this.props.value,
+        formattedNumber: input.value,
       });
     }
   }
